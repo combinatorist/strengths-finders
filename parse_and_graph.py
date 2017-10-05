b5b92d9951fa57fdec2418938a0da02875415a67
Tim Eccleston <teccleston@axialhealthcare.com>
2017-10-04 20:24:47 -0500

    stop using 'rank' / 'value' as edge label

--- parse_and_graph.py
+++ parse_and_graph.py
@@ -8,7 +8,7 @@ melted_df = pd.melt(raw_df.reset_index(), id_vars=['index']).dropna()
 strings_df = melted_df.applymap(str)
 
 graph = gv.Graph()
-graph_renaming = {'index':'tail_name','variable':'head_name','value':'label'}
+graph_renaming = {'index':'tail_name','variable':'head_name','value':'rank'}
 for edge in strings_df.rename(columns=graph_renaming).to_dict('records'):
     graph.edge(**edge)
 graph.engine = 'neato'
