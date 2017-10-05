f1820a497cf7c95dc732ca9ad52d2a7eb9fe63f5
Tim Eccleston <teccleston@axialhealthcare.com>
2017-10-04 20:35:19 -0500

    attempt weighting by node attribute: length (small difference)

--- parse_and_graph.py
+++ parse_and_graph.py
@@ -5,6 +5,7 @@ FILEPATH = 'executive_team_raw.csv'
 
 raw_df = pd.DataFrame.from_csv(FILEPATH, header=1)
 melted_df = pd.melt(raw_df.reset_index(), id_vars=['index']).dropna()
+weights_df = melted_df + (melted_df['value']).to_frame('length')
 strings_df = melted_df.applymap(str)
 
 graph = gv.Graph()
@@ -12,6 +13,6 @@ graph_renaming = {'index':'tail_name','variable':'head_name','value':'rank'}
 for edge in strings_df.rename(columns=graph_renaming).to_dict('records'):
     graph.node(edge['tail_name'], fillcolor='lightblue', style='filled')
     graph.edge(**edge)
-graph.engine = 'circo'
+graph.engine = 'neato'
 graph.graph_attr['overlap'] = 'false'
 graph.render(view=True)
